d0717cbec4fe10e524ba6654645fc6c6b9641da2
Nikita <69010558+Kapibarra@users.noreply.github.com>
2021-08-31 14:41:20 +0300

    added store redux

--- src/index.js
+++ src/index.js
@@ -2,11 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+// Redux 
+import { createStore } from 'redux'
+import rootReducer from './reducers'
+import { Provider } from 'react-redux';
+
+const store = createStore(rootReducer)
 
 ReactDOM.render(
+
   <React.StrictMode>
+    <Provider store={store}>
     <App />
-  </React.StrictMode>,
+    </Provider>
+  </React.StrictMode>
+,
   document.getElementById('root')
 );
 
